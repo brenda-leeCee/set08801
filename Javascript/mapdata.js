21b37fcfb4b19bc831ba177c51affa4121dd80ce
brenda-leeCee <40734342@live.napier.ac.uk>
2025-04-23 13:42:28 -0600

    Map JS

---
+++ Javascript/mapdata.js
@@ -0,0 +1,242 @@
+var simplemaps_europemap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    popups: "detect",
+    state_description: "",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    manual_zoom: "no",
+    back_image: "no",
+    arrow_color: "#cecece",
+    arrow_color_border: "#808080",
+    initial_back: "no",
+    initial_zoom: -1,
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    AL: {
+      name: "Albania",
+      color: "#930ca2"
+    },
+    AM: {
+      name: "Armenia",
+      color: "#c75454"
+    },
+    AT: {
+      name: "Austria",
+      color: "#54c759"
+    },
+    BA: {
+      name: "Bosnia and Herzegovina",
+      color: "#2376c0"
+    },
+    BE: {
+      name: "Belgium",
+      color: "#23c09e"
+    },
+    BG: {
+      name: "Bulgaria",
+      color: "#2ec023"
+    },
+    BY: {
+      name: "Belarus",
+      color: "#72af00"
+    },
+    CH: {
+      name: "Switzerland",
+      color: "#af007c"
+    },
+    CY: {
+      name: "Cyprus",
+      color: "#9c00af"
+    },
+    CZ: {
+      name: "Czech Republic",
+      color: "#7300d1"
+    },
+    DE: {
+      name: "Germany",
+      color: "#3700d1"
+    },
+    DK: {
+      name: "Denmark",
+      color: "#00d18b"
+    },
+    EE: {
+      name: "Estonia",
+      color: "#28d100"
+    },
+    ES: {
+      name: "Spain",
+      color: "#d12b00"
+    },
+    FI: {
+      name: "Finland",
+      color: "#d10058"
+    },
+    FR: {
+      name: "France",
+      color: "#8b00d1"
+    },
+    GB: {
+      name: "United Kingdom",
+      color: "#00cfd1"
+    },
+    GE: {
+      name: "Georgia",
+      color: "#64d100"
+    },
+    GR: {
+      name: "Greece",
+      color: "#f46060"
+    },
+    HR: {
+      name: "Croatia",
+      color: "#ffcbea"
+    },
+    HU: {
+      name: "Hungary",
+      color: "#d9cbff"
+    },
+    IE: {
+      name: "Ireland",
+      color: "#cbe1ff"
+    },
+    IS: {
+      name: "Iceland",
+      color: "#dbffcb"
+    },
+    IT: {
+      name: "Italy",
+      color: "#ffcbcb"
+    },
+    LT: {
+      name: "Lithuania",
+      color: "#b0c22c"
+    },
+    LU: {
+      name: "Luxembourg",
+      color: "#27d537"
+    },
+    LV: {
+      name: "Latvia",
+      color: "#27d5a4"
+    },
+    MD: {
+      name: "Moldova",
+      color: "#1a795e"
+    },
+    ME: {
+      name: "Montenegro",
+      color: "#a4af00"
+    },
+    MK: {
+      name: "Macedonia",
+      color: "#00af10"
+    },
+    NL: {
+      name: "Netherlands",
+      color: "#00860c"
+    },
+    NO: {
+      name: "Norway",
+      color: "#f87373"
+    },
+    PL: {
+      name: "Poland",
+      color: "#2da47c"
+    },
+    PT: {
+      name: "Portugal",
+      color: "#c0d33b"
+    },
+    RO: {
+      name: "Romania",
+      color: "#d33b3b"
+    },
+    RS: {
+      name: "Serbia",
+      color: "#17a8a9"
+    },
+    SE: {
+      name: "Sweden",
+      color: "#c4773d"
+    },
+    SI: {
+      name: "Slovenia",
+      color: "#c43d3d"
+    },
+    SK: {
+      name: "Slovakia",
+      color: "#c75454"
+    },
+    TR: {
+      name: "Turkey",
+      color: "#b62cc5"
+    },
+    UA: {
+      name: "Ukraine",
+      color: "#930ca2"
+    }
+  },
+  locations: {},
+  regions: {},
+  labels: {},
+  legend: {
+    entries: []
+  }
+};
